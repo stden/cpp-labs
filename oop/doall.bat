09b5f071827b60fe65741d608a7f014b927c5bbe
super.denis <super.denis@localhost>
2010-09-28 14:04:24 +0000

    ????????????? ?????????????? :)

---
+++ oop/doall.bat
@@ -0,0 +1,12 @@
+call astyle.bat lab2 h
+call astyle.bat lab2 cpp
+del "lab2\*.orig"
+call astyle.bat lab3 h
+call astyle.bat lab3 cpp
+del "lab3\*.orig"
+call astyle.bat lab4 h
+call astyle.bat lab4 cpp
+del "lab4\*.orig"
+call astyle.bat kurs h
+call astyle.bat kurs cpp
+del "kurs\*.orig"
